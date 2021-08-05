0a894e556b204fd4e551fec987d4f77d35c51196
PeeQuessada <pedro.rafael.quessada@gmail.com>
2021-08-05 16:04:24 -0300

    Challange 11

--- force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
+++ force-app/main/default/lwc/boatAddReviewForm/boatAddReviewForm.js
@@ -1,11 +1,16 @@
 // imports
 import { LightningElement, api, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+
 // import BOAT_REVIEW_OBJECT from schema - BoatReview__c
 import BOAT_REVIEW_OBJECT from '@salesforce/schema/BoatReview__c';
 // import NAME_FIELD from schema - BoatReview__c.Name
 import NAME_FIELD from '@salesforce/schema/BoatReview__c.Name';
 // import COMMENT_FIELD from schema - BoatReview__c.Comment__c
 import COMMENT_FIELD from '@salesforce/schema/BoatReview__c.Comment__c';
+
+const SUCCESS_TITLE = 'Review Created!';
+const SUCCESS_VARIANT = 'success';
 export default class BoatAddReviewForm extends LightningElement {
   // Private
   boatId;
@@ -29,21 +34,50 @@ export default class BoatAddReviewForm extends LightningElement {
   }
   
   // Gets user rating input from stars component
-  handleRatingChanged(event) { }
+  handleRatingChanged(event) { 
+    const rating = event.detail.rating;
+    this.rating = rating;
+  }
   
   // Custom submission handler to properly set Rating
   // This function must prevent the anchor element from navigating to a URL.
   // form to be submitted: lightning-record-edit-form
-  handleSubmit(event) { }
+  handleSubmit(event) {
+    event.preventDefault();       // stop the form from submitting
+
+    const fields = event.detail.fields;
+    fields.Boat__c = this.boatId;
+    fields.Rating__c = this.rating;
+
+    this.template.querySelector('lightning-record-edit-form').submit(fields);
+  
+  }
   
   // Shows a toast message once form is submitted successfully
   // Dispatches event when a review is created
   handleSuccess() {
     // TODO: dispatch the custom event and show the success message
+    this.dispatchEvent(new CustomEvent('createreview'));
+
+    this.dispatchEvent(
+      new ShowToastEvent({
+        title : SUCCESS_TITLE,
+        message : '',
+        variant : SUCCESS_VARIANT
+      })
+    );
+
     this.handleReset();
   }
   
   // Clears form data upon submission
   // TODO: it must reset each lightning-input-field
-  handleReset() { }
+  handleReset() {
+    this.boatId = null;
+    this.rating = null;
+
+    this.template.querySelectorAll('lightning-input-field').forEach(function(field){      
+      field.reset()
+    });
+  }
 }
