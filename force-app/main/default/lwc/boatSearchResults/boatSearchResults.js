a607f46c118a69f8c28ea004b133cdbc623933bb
PeeQuessada <pedro.rafael.quessada@gmail.com>
2021-08-05 10:53:29 -0300

    challange 6

--- force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
+++ force-app/main/default/lwc/boatSearchResults/boatSearchResults.js
@@ -2,6 +2,9 @@ import { LightningElement, api, wire, track } from 'lwc';
 import getBoats from '@salesforce/apex/BoatDataService.getBoats';
 import updateBoatList from '@salesforce/apex/BoatDataService.updateBoatList';
 
+import { APPLICATION_SCOPE,subscribe,MessageContext } from 'lightning/messageService';
+import BOATMC from '@salesforce/messageChannel/BoatMessageChannel__c';
+
 import { refreshApex } from '@salesforce/apex';
 import { getRecordNotifyChange } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent'
@@ -20,14 +23,22 @@ const ERROR_TITLE = 'Error';
 const ERROR_VARIANT = 'error';
 
 export default class BoatSearchResults extends LightningElement {
+  selectedBoatId;
+  columns = COLUMNS;
+
   @api boatTypeId = '';
   @track boats;
-  isLoading;
-  selectedBoatId;
 
-  columns = COLUMNS;
+  isLoading;
   saveDraftValues = [];
 
+  // wired message context
+  messageContext;
+
+  // Initialize messageContext for Message Service
+  @wire(MessageContext) messageContext;
+
+  // wired getBoats method (OK)
   @wire(getBoats, { boatTypeId: '$boatTypeId'})  
   wiredBoats({ error, data }) {
     if(data) {
@@ -35,7 +46,6 @@ export default class BoatSearchResults extends LightningElement {
       this.dispatchEvent(new CustomEvent('loading'));
       console.log('Data ', JSON.stringify(data));
       this.boats = data;
-
 
       this.dispatchEvent(new CustomEvent('doneloading'));
       this.isLoading = false;
@@ -50,14 +60,17 @@ export default class BoatSearchResults extends LightningElement {
     this.selectedBoatId = detail.boatId;
   }
 
+  // The handleSave method must save the changes in the Boat Editor (OK)
+  // passing the updated fields from draftValues to the 
+  // Apex method updateBoatList(Object data).
+  // Show a toast message with the title
+  // clear lightning-datatable draft values
   async handleSave(event) {
-    console.log('save')
     this.saveDraftValues = event.detail.draftValues;
     const notifyChangeIds = this.saveDraftValues.map(row => { return { "recordId": row.Id } });
 
     await updateBoatList({ data: this.saveDraftValues })
       .then((result) => {
-        console.log('result success');
         refreshApex(this.wiredBoats);
         this.dispatchEvent(
           new ShowToastEvent({
